98207bc5e7d301942ac83f40be2f630da3c647c3
Jeroen de Lange <106904476+delangejh@users.noreply.github.com>
2022-06-05 11:10:07 +0200

    Update streamlit_app.py
    Added emoji's and adjusted text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('omega 3 and blueberry outmeal')
-streamlit.text('kale, spinach and rocket smoothie')
-streamlit.text('hard boiled free range egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Outmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')

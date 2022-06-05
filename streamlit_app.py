d9ce0e196a7d17963733e821424a8b01ac020cdc
Jeroen de Lange <106904476+delangejh@users.noreply.github.com>
2022-06-05 11:02:26 +0200

    Update streamlit_app.py
    Added a header and three text rows by jle

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('omega 3 and blueberry outmeal')
+streamlit.text('kale, spinach and rocket smoothie')
+streamlit.text('hard boiled free range egg')

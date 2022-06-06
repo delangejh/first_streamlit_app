26f2902d08000bb1193fad23e4c4160c27564887
Jeroen de Lange <106904476+delangejh@users.noreply.github.com>
2022-06-06 10:23:41 +0200

    Update streamlit_app.py
    addition of normalized response and streamlit dataframe on screen

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,9 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #this just writes data on the screen
+
+#take the json version of the resonse and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_resonse.json())
+#output is on the screen as a table
+streamlit.dataframe(fruityvice_normalized)

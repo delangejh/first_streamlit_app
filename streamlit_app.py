8d1ee56cdcac83c0604d79e130e3d0d4de3611a9
Jeroen de Lange <106904476+delangejh@users.noreply.github.com>
2022-06-06 18:38:52 +0200

    Update streamlit_app.py
    adjust typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ try:
   if not fruit_choise:
     streamlit.error("Please select a fruit to get info.")
   else:
-    back_from_function = get_fruityvice_data(fruit_choice)
+    back_from_function = get_fruityvice_data(this_fruit_choice)
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()

c9536358d452a94f04a32647d682779e6b63df20
Jeroen de Lange <106904476+delangejh@users.noreply.github.com>
2022-06-06 17:58:05 +0200

    Update streamlit_app.py
    adjust indent after warning message

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,14 +42,14 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-  try:
-    fruit_choise = streamlit.text_input('What fruit do you like info about?')
-    if not fruit_choise:
-      streamlit.error("Please select a fruit to get info.")
-    else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choise)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choise = streamlit.text_input('What fruit do you like info about?')
+  if not fruit_choise:
+    streamlit.error("Please select a fruit to get info.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choise)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 
